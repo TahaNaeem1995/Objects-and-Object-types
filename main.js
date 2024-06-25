9e21a676e225006263e38762b67779db6d1c9481
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 16:00:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+"use strict";
+// const myProperties = {
+//     address: "Bhool gaya",
+//     name: "Taha",
+//     age: 28,
+//     rollNo: 11204,
+// }
+// console.log(myProperties["age"]);
+// const myProperties ={
+//     name: "taha",
+//     age: 28,
+//     rollNo: 11204,
+// }
+// // console.log(myproperties);
+// const userPrompt = "name"
+// console.log(myProperties);
+// const myProperties ={
+//     address:{
+//         city:"Karachi",
+//         country: "Pakistan",
+//         postalCode:undefined,
+//     },
+//     name: "taha",
+//     age: 28,
+//     rollNo: 11204,
+// }
+// console.log(myProperties.address.country);
+//const userPrompt = "name"
+let myProperties = {
+    name: "Taha",
+    age: 28,
+    address: "bhool gaya",
+    rollNo: 11204,
+};
+console.log(myProperties);
